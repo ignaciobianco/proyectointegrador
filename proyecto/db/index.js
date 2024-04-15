5cd12fa15e0225a3385c3057618e0271d4de798b
ignaciobianco <ibianco@udesa.edu.ar>
2024-04-15 15:56:48 -0300

    db

--- proyecto/db/index.js
+++ proyecto/db/index.js
@@ -6,23 +6,59 @@ const datos = {
       contraseña: "orlando",
       fecha_nacimiento: "2023-11-10",
       dni: "46789024",
-      foto_perfil: "images/users/default-image.png"
+      foto_perfil: "bpeleritti.jpg"
     },
-    productos: [
+    productos : [
       {
-        id: 1,
-        imagen_producto: "/img/producto1.jpg",
-        nombre_producto: "Producto 1",
-        descripcion_producto: "Descripción del Producto 1",
-        comentarios: [
-          {
-            usuario: "ejemplo",
-            texto_comentario: "Comentario de ejemplo",
-            imagen_perfil: "/img/perfil.jpg"
-          }
-        ]
+        imagen: "imagen1.jpg",
+        nombre: "Producto 1",
+        descripcion: "Descripción del Producto 1"
       },
-      // Aquí se agregan más productos...
+      {
+        imagen: "imagen2.jpg",
+        nombre: "Producto 2",
+        descripcion: "Descripción del Producto 2"
+      },
+      {
+        imagen: "imagen3.jpg",
+        nombre: "Producto 3",
+        descripcion: "Descripción del Producto 3"
+      },
+      {
+        imagen: "imagen4.jpg",
+        nombre: "Producto 4",
+        descripcion: "Descripción del Producto 4"
+      },
+      {
+        imagen: "imagen5.jpg",
+        nombre: "Producto 5",
+        descripcion: "Descripción del Producto 5"
+      },
+      {
+        imagen: "imagen6.jpg",
+        nombre: "Producto 6",
+        descripcion: "Descripción del Producto 6"
+      },
+      {
+        imagen: "imagen7.jpg",
+        nombre: "Producto 7",
+        descripcion: "Descripción del Producto 7"
+      },
+      {
+        imagen: "imagen8.jpg",
+        nombre: "Producto 8",
+        descripcion: "Descripción del Producto 8"
+      },
+      {
+        imagen: "imagen9.jpg",
+        nombre: "Producto 9",
+        descripcion: "Descripción del Producto 9"
+      },
+      {
+        imagen: "imagen10.jpg",
+        nombre: "Producto 10",
+        descripcion: "Descripción del Producto 10"
+      }
     ]
   };
   
