2a8fafa8ae01b31680e654d8cf6830d622d0f447
ignaciobianco <ibianco@udesa.edu.ar>
2024-06-22 14:55:21 -0300

    Edit product terminado

--- proyecto/controllers/productController.js
+++ proyecto/controllers/productController.js
@@ -14,35 +14,24 @@ const productController = {
         db.Producto.findByPk(productId, {
 
             include: [{
-                association: 'usuario' 
+                association: 'usuario'
             }, {
-                association: 'comentarios' , include: [{ association: 'usuario'}]
+                association: 'comentarios', include: [{ association: 'usuario' }]
             }]
-                
+
         })
-        .then(function(productos){
-            
-            return res.render('product', { 
-                
-                producto: productos,
-                comentarios: productos.comentarios
-            });
-        }
-        )
+            .then(function (productos) {
 
-    //     const primerProducto = db.productos[0]; // Obtener el primer producto
-    //     const comentarios = [ // Extraer los primeros tres comentarios
-    //         primerProducto.comentarios[0],
-    //         primerProducto.comentarios[1],
-    //         primerProducto.comentarios[2],
-    //     ];
+                return res.render('product', {
 
-    //     return res.render('product', {
-    //         producto: primerProducto,
-    //         comentarios,
-    //     })
+                    producto: productos
+                    
+                });
+            }
+            )
 
-     },
+
+    },
 
 
 
@@ -56,7 +45,51 @@ const productController = {
         })
     },
 
+    editProduct: function (req, res) {
 
+
+        let id = req.params.id;
+        // return res.send(id)
+        db.Producto.findByPk(id, {
+            include: [{ association: 'usuario' }]
+        })
+            .then(function (result) {
+                // return res.send(result)
+                res.render("product-edit", { productoEdit: result })
+            })
+
+
+    },
+    update: function (req, res) {
+        let id = req.params.id;
+        form = req.body
+        console.log('Datos del formulario:', form);
+
+        let datosActualizados = {
+
+            nombre_producto: form.nombre_producto,
+            descripcion_producto: form.descripcion_producto,
+            imagen_producto: form.imagen_producto
+
+        };
+
+        // return res.send(id)
+        db.Producto.update(datosActualizados, { where: [{ id: id }] })
+            .then(function (result) {
+                console.log('Resultado de la actualización:', result);
+                return res.redirect('/product/' + id)
+
+
+            }).catch(function (error) {
+                console.log(error);
+            })
+
+
+
+
+
+
+    },
 
 
     resultadosBusqueda: function (req, res) {
--- proyecto/routes/product.js
+++ proyecto/routes/product.js
@@ -8,8 +8,12 @@ const productController = require('../controllers/productController')
 
 router.get('/product-add', productController.addProduct);
 router.get('/', productController.productos);
+
 router.get('/search-results', productController.resultadosBusqueda);
+
 router.get('/:id', productController.productos);
 
+router.get("/product-edit/:id",productController.editProduct)
+router.post("/product-edit/:id",productController.update)
 
 module.exports = router;
